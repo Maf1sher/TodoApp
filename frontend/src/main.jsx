ec4058c058f88cf6832f7e27530879efb72ffdec
JakubL <kubas307@gameil.com>
2024-12-16 08:35:11 +0100

    BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home.jsx";
+import {Login} from "./pages/Login.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <React.StrictMode>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+    </React.StrictMode>
 )
